125c12a817b0b0856e392e4bfcea2582aeafba9d
Ayush-0312 <kumarayush3121511@gmail.com>
2025-04-01 14:25:51 +0530

    fixed Shrimmer

--- src/components/Shrimmer.js
+++ src/components/Shrimmer.js
@@ -1,22 +1,12 @@
 const Shrimmer = () => {
   return (
-    
-    <div className="shrimmer-container">
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
-      <div className="shrimmer-cards"></div>
+    <div className="flex flex-wrap gap-8 p-8 bg-rose-50">
+      {Array.from({ length: 15 }).map((_, index) => (
+        <div
+          key={index}
+          className="w-48 h-72 bg-rose-100 rounded-lg shadow-xl animate-pulse"
+        ></div>
+      ))}
     </div>
   );
 };
