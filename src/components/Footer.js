eb5c93cdf448605aa441ab8b5c8e4b3d7980f896
Ayush-0312 <kumarayush3121511@gmail.com>
2025-04-01 13:47:11 +0530

    updated links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { AVATAR } from "../utils/constants";
+import { AVATAR, PORTFOLIO } from "../utils/constants";
 import { FaGithub } from "react-icons/fa6";
 import { GITHUB, LINKEDIN } from "../utils/constants";
 import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
+  const handleAvatarClick = () => {
+    window.open(PORTFOLIO, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <div className="mt-auto flex bg-pink-200 p-5 justify-between">
       <div>
@@ -12,7 +16,13 @@ const Footer = () => {
       </div>
       <div className="flex space-x-4">
         <span className="font-semibold text-lg">Created by- Ayush Gupta</span>
-        <img className="w-8 rounded-xl" src={AVATAR} alt="avatar" />
+        <div onClick={handleAvatarClick}>
+          <img
+            className="w-8 rounded-xl cursor-pointer"
+            src={AVATAR}
+            alt="avatar"
+          />
+        </div>
         <a href={GITHUB} target="_blank" rel="noopener noreferrer">
           <FaGithub size={30} />
         </a>
